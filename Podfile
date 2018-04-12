5539694f4b8391240c4a4cb67280cbb226d3b69e
Elderied Mckinney <mckinneydemetrice@icloud.com>
2018-04-11 23:31:10 -0400

    added new pods

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '11.0'
+
+target 'SmackZ' do
+  use_frameworks!
+
+#Pods for SmackZ
+
+pod  'Alamofire'
+pod  'SwiftyJSON'
+pod  'Socket.IO-Client-Swift'
+
+end
---
+++ build/Pods.build/Debug-iphoneos/Pods-SmackZ.build/DerivedSources/Pods_SmackZ_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_SmackZVersionString[];
+ extern const double Pods_SmackZVersionNumber;
+
+ const unsigned char Pods_SmackZVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_SmackZ  PROJECT:Pods-1" "\n";
+ const double Pods_SmackZVersionNumber __attribute__ ((used)) = (double)1.;
